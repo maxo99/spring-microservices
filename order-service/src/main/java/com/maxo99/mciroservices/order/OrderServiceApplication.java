c39a561c4e767773169d1d879bc42d6756853093
maxo <max.orr@mail.com>
2025-09-28 06:49:06 -0400

    feat(order-service): inclusion of spring-initializer project

---
+++ order-service/src/main/java/com/maxo99/mciroservices/order/OrderServiceApplication.java
@@ -0,0 +1,13 @@
+package com.maxo99.mciroservices.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+	}
+
+}
