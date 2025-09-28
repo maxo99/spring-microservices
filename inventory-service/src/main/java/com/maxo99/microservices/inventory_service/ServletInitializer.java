48c13a51ccc5a3afe973870a7d4dabbd31b06fcf
maxo <max.orr@mail.com>
2025-09-28 10:06:29 -0400

    feat(inventory-service): initializing inventory-service from spring init

---
+++ inventory-service/src/main/java/com/maxo99/microservices/inventory_service/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.maxo99.microservices.inventory_service;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(InventoryServiceApplication.class);
+	}
+
+}
