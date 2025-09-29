be9f9a6e91955fe9987046ffac22837862ec165f
maxo <max.orr@mail.com>
2025-09-29 18:40:39 -0400

    feat(circuit-breaker): including resilience4j at order layer

--- order-service/src/main/java/com/maxo99/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/maxo99/microservices/order/client/InventoryClient.java
@@ -1,11 +1,26 @@
 package com.maxo99.microservices.order.client;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.service.annotation.GetExchange;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.retry.annotation.Retry;
+
 public interface InventoryClient {
 
+    Logger log = LoggerFactory.getLogger(InventoryClient.class);
+
     @GetExchange()
+    @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackDefault")
+    @Retry(name = "inventory")
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
 
+
+
+    default boolean fallbackDefault(String skuCode, Integer quantity, Throwable throwable) {
+        log.warn("Inventory service is down. Fallback method called for SKU: {} Failure Reason: {}", skuCode, throwable.getMessage());
+        return false;
+    }
 }
--- order-service/src/main/java/com/maxo99/microservices/order/config/RestClientConfig.java
+++ order-service/src/main/java/com/maxo99/microservices/order/config/RestClientConfig.java
@@ -2,6 +2,8 @@ package com.maxo99.microservices.order.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
 import org.springframework.web.service.invoker.HttpServiceProxyFactory;
@@ -12,19 +14,26 @@ import org.springframework.beans.factory.annotation.Value;
 
 @Configuration
 public class RestClientConfig {
- 
+
     @Value("${inventory.url}")
     private String inventoryServiceUrl;
-
-
 
     @Bean
     public InventoryClient inventoryClient() {
         RestClient restClient = RestClient.builder()
                 .baseUrl(inventoryServiceUrl)
+                .requestFactory(getRequestFactory())
                 .build();
         var restClientAdapter = RestClientAdapter.create(restClient);
         var httpServiceProxyFactory = HttpServiceProxyFactory.builderFor(restClientAdapter).build();
         return httpServiceProxyFactory.createClient(InventoryClient.class);
     }
+
+    private ClientHttpRequestFactory getRequestFactory() {
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(3000);
+        factory.setReadTimeout(3000);
+        return factory;
+    }
+
 }
