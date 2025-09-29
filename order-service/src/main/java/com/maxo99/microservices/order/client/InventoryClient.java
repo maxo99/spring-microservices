b3800598e3b89a759aae435f54445e125dab8ddc
maxo <max.orr@mail.com>
2025-09-29 09:56:13 -0400

    refactor(restclient): removing deprecated openfeign for rest client

--- order-service/src/main/java/com/maxo99/microservices/order/client/InventoryClient.java
+++ order-service/src/main/java/com/maxo99/microservices/order/client/InventoryClient.java
@@ -1,13 +1,11 @@
 package com.maxo99.microservices.order.client;
 
-import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.service.annotation.GetExchange;
 
-@FeignClient(name = "inventory", url = "${inventory.url}")
 public interface InventoryClient {
-    @RequestMapping(method = RequestMethod.GET)
+
+    @GetExchange()
     boolean isInStock(@RequestParam String skuCode, @RequestParam Integer quantity);
-    
+
 }
