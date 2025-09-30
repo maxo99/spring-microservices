df9043d5eed56f325da24d70bfd90d71d6ed29db
maxo <max.orr@mail.com>
2025-09-30 12:59:28 -0400

    feat(kafka): introducing the notification service

---
+++ notification-service/src/test/java/com/maxo99/microservices/notification/TestNotificationServiceApplication.java
@@ -0,0 +1,11 @@
+package com.maxo99.microservices.notification;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(NotificationServiceApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
