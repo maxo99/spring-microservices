e38d8817127941f7a229a2c551f21fd6c6f49459
maxo <max.orr@mail.com>
2025-09-30 05:58:43 -0400

    feat(frontend): configuring header component

--- fontend/microservices-shop-frontend/src/app/shared/header/header.ts
+++ fontend/microservices-shop-frontend/src/app/shared/header/header.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
+import {OidcSecurityService} from "angular-auth-oidc-client";
 
 @Component({
   selector: 'app-header',
+  standalone: true,
   imports: [],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
-export class Header {
+export class HeaderComponent implements OnInit {
 
+  private readonly oidcSecurityService = inject(OidcSecurityService);
+  isAuthenticated = false;
+  username = "";
+
+  ngOnInit(): void {
+    this.oidcSecurityService.isAuthenticated$.subscribe(
+      ({isAuthenticated}) => {
+        this.isAuthenticated = isAuthenticated;
+      }
+    )
+    this.oidcSecurityService.userData$.subscribe(
+      ({userData}) => {
+        this.username = userData.preferred_username
+      }
+    )
+  }
+
+  login(): void {
+    this.oidcSecurityService.authorize();
+  }
+
+  logout(): void {
+    this.oidcSecurityService
+      .logoff()
+      .subscribe((result) => console.log(result));
+  }
 }
