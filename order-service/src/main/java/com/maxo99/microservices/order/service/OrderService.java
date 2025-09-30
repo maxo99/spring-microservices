edfac97fecb7a547b32e92d03b9991c37975f893
maxo <max.orr@mail.com>
2025-09-30 12:58:50 -0400

    feat(kafka): adding order-place topic publishing

--- order-service/src/main/java/com/maxo99/microservices/order/service/OrderService.java
+++ order-service/src/main/java/com/maxo99/microservices/order/service/OrderService.java
@@ -3,11 +3,14 @@ package com.maxo99.microservices.order.service;
 import java.util.List;
 import java.util.UUID;
 
+import org.slf4j.Logger;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.maxo99.microservices.order.client.InventoryClient;
 import com.maxo99.microservices.order.dto.OrderRequest;
 import com.maxo99.microservices.order.dto.OrderResponse;
+import com.maxo99.microservices.order.event.OrderPlacedEvent;
 import com.maxo99.microservices.order.model.Order;
 import com.maxo99.microservices.order.repository.OrderRepository;
 
@@ -16,9 +19,10 @@ import lombok.RequiredArgsConstructor;
 @Service
 @RequiredArgsConstructor
 public class OrderService {
-
+    private static final Logger log = org.slf4j.LoggerFactory.getLogger(OrderService.class);
     private final OrderRepository orderRepository;
     private final InventoryClient inventoryClient;
+    private final KafkaTemplate<String, OrderPlacedEvent> kafkaTemplate;
 
     public void placeOrder(OrderRequest orderRequest) {
         var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
@@ -31,7 +35,15 @@ public class OrderService {
         order.setPrice(orderRequest.price());
         order.setSkuCode(orderRequest.skuCode());
         order.setQuantity(orderRequest.quantity());
+
+        OrderPlacedEvent event = new OrderPlacedEvent(order.getOrderNumber(), orderRequest.userDetails().email());
+        log.info("OrderPlacedEvent:{} sending", event.getOrderNumber());
+        kafkaTemplate.send("order-placed", event);
+        log.info("OrderPlacedEvent:{} sent", event.getOrderNumber());
+
+
         orderRepository.save(order);
+        log.info("Order with id {} is saved to DB", order.getId());
     }
 
     public OrderResponse getOrder(Long orderId) {
