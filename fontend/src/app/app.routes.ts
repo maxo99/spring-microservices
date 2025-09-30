bc8f55dc32a99a9326fb4866bceb50bcdcca24c0
maxo <max.orr@mail.com>
2025-09-30 06:32:50 -0400

    feat(frontend): adding app routes for add-product and home-page

--- fontend/src/app/app.routes.ts
+++ fontend/src/app/app.routes.ts
@@ -1,3 +1,8 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {HomePageComponent} from "./pages/home-page/home-page";
+import {AddProductComponent} from "./pages/add-product/add-product";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component: HomePageComponent},
+  {path: 'add-product', component: AddProductComponent}
+];
