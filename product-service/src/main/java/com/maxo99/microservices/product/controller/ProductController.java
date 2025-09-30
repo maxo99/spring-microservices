ca1666a7dcbe5ab82c8374a7de602a201c7e92be
maxo <max.orr@mail.com>
2025-09-30 09:07:28 -0400

    chore: cleaning up repo warnings

--- product-service/src/main/java/com/maxo99/microservices/product/controller/ProductController.java
+++ product-service/src/main/java/com/maxo99/microservices/product/controller/ProductController.java
@@ -8,10 +8,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.maxo99.microservices.product.dto.ProductIdResponse;
 import com.maxo99.microservices.product.dto.ProductRequest;
 import com.maxo99.microservices.product.dto.ProductResponse;
-import com.maxo99.microservices.product.model.Product;
 import com.maxo99.microservices.product.service.ProductService;
 
 import lombok.RequiredArgsConstructor;
